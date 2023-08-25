82d5c6175b7e2f8a003c871de7ae9c4e4a431c43
Seasawher <tellagoodstory@icloud.com>
2023-08-25 20:36:30 +0900

    add description to make Solution file

--- Solution.lean
+++ Solution.lean
@@ -1,1 +1,15 @@
-def solution : Nat := 0
+/-
+
+## How to use
+
+* uncomment `#type_hash` and `import` lines to get hash of the solution
+* run `lake exe grade {hash of the solution}` in your terminal
+
+-/
+
+-- import LeanGrader.Basic
+
+def solution : 1 + 1 = 2 := by
+  sorry
+
+-- #type_hash solution
